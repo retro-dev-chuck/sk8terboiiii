e89d233f271406ea3404d70bd7fb718a266c8818
Chuckiee3 <81488943+retro-dev-chuck@users.noreply.github.com>
2025-04-06 21:21:46 +0200

    Some sfx for environment elements

--- NotSoCoolPlayer/Torch/PlayerTorch.gd
+++ NotSoCoolPlayer/Torch/PlayerTorch.gd
@@ -7,6 +7,8 @@ const FRESH_LIFETIME: float = 20.0
 @export var lifetime: Lifetime
 @export var pfx: GPUParticles3D
 @export var light: Node3D
+@export var audio_player: AudioStreamPlayer3D
+@export var audio_player_extinguish: AudioStreamPlayer3D
 
 var is_live: bool = false
 
@@ -18,15 +20,23 @@ func _extinguish() -> void:
 	pfx.visible = false
 	light.visible = false
 	is_live = false
+	audio_player.stop()
 	
 func _hide() -> void:
 	##todo: play vfx, sfx here
 	## create empty throw on ground
 	on_expire.emit()
+	audio_player.stop()
+	if audio_player_extinguish:
+		audio_player_extinguish.stop()
 	is_live = false
 	visible = false
 
 func refresh(time: float = FRESH_LIFETIME) -> void:
+	if not audio_player.playing:
+		audio_player.play()
+	if audio_player_extinguish.playing:
+		audio_player_extinguish.stop()
 	pfx.amount = 16
 	lifetime.start(time)
 	is_live = true
